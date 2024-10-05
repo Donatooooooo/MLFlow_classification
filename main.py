e52eb6a71da7950db2875c5b73b0375214ecd5d4
donatooooooo <donatoboccuzzi@gmail.com>
2024-10-05 11:48:18 +0200

    new main

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 from Dataset.dataset import Dataset
 from classifier import ModelTrainerClass
-from kmeans import kMeans
-from MLFlow import trainAndLog
-
-def preprocessing(dataset : Dataset):
-    dataset.dropDatasetColumns(["id"])
-    dataset.replaceBoolean("M", "B")
-    for column in dataset.getDataset().columns:
-        dataset.normalizeColumn(column)
-    
-    features = dataset.getDataFrame(['radius_mean', 'texture_mean', 'perimeter_mean'])
-    kmeans = kMeans().clustering(features)
-    dataset.addDatasetColumn('Appearance Cluster', kmeans.fit_predict(features))
-    dataset.dropDatasetColumns(columnsToRemove=['radius_mean', 'texture_mean', 'perimeter_mean'])
-    dataset.normalizeColumn('Appearance Cluster')
-    return dataset
+from MLFlow import trainAndLog, createMD
+from util.util import preprocessing
 
 
-# dataset = Dataset("Dataset/brest_cancer.csv")
-# dataset = preprocessing(dataset)
+dataset = Dataset("Dataset/brest_cancer.csv")
+dataset = preprocessing(dataset)
 
-# trainer = ModelTrainerClass('diagnosis', ['diagnosis'], dataset)
-# trainAndLog(dataset, trainer, "RFclassifier v2", "Random Forest")
+trainer = ModelTrainerClass('diagnosis', ['diagnosis'], dataset)
+trainAndLog(dataset, trainer, "RFclassifier v2", "brest_cancer.csv", "Random Forest")
+
+createMD("Random Forest", 9)
