64176e787cecafe5f5c094dca2f83226498fa91d
donatooooooo <donatoboccuzzi@gmail.com>
2024-10-11 12:21:11 +0200

    testing comment extractor

--- src/main.py
+++ src/main.py
@@ -39,3 +39,19 @@ if False:
     ModelCard("KNN Classifier", 3)
     ModelCard("Random forest with kMeans", 15)
     ModelCard("Random Forest Classifier", 3)
+
+import sys
+
+def process_string(input_string):
+    result = input_string.upper()  # Qui semplicemente converto la stringa in maiuscolo
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso corretto: python main.py <stringa>")
+        sys.exit(1)
+
+    input_string = sys.argv[1]
+    result = process_string(input_string)
+    
+    print(f"Risultato: {result}")
