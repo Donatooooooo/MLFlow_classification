39556635eac8c3c9a273b3b6a3f64a05478eac5a
donatooooooo <donatoboccuzzi@gmail.com>
2024-10-11 16:27:00 +0200

    test4

--- src/Utils/utility.py
+++ src/Utils/utility.py
@@ -1,23 +1,21 @@
 from datetime import datetime
-from Utils.kmeans import kMeans
-from Dataset.dataset import Dataset
-import mlflow, json, numpy as np, pandas as pd
+import json
 
-def preprocessing(dataset : Dataset, cluster = False):
-    dataset.dropDatasetColumns(["id"])
-    dataset.replaceBoolean("M", "B")
-    for column in dataset.getDataset().columns:
-        dataset.normalizeColumn(column)
+# def preprocessing(dataset : Dataset, cluster = False):
+#     dataset.dropDatasetColumns(["id"])
+#     dataset.replaceBoolean("M", "B")
+#     for column in dataset.getDataset().columns:
+#         dataset.normalizeColumn(column)
     
-    if cluster:
-        features = dataset.getDataFrame(['radius_mean', 'texture_mean', 'perimeter_mean'])
-        kmeans = kMeans().clustering(features)
-        dataset.addDatasetColumn('Appearance Cluster', kmeans.fit_predict(features))
-        dataset.dropDatasetColumns(columnsToRemove=['radius_mean', 'texture_mean', 'perimeter_mean'])
-        dataset.normalizeColumn('Appearance Cluster')
+#     if cluster:
+#         features = dataset.getDataFrame(['radius_mean', 'texture_mean', 'perimeter_mean'])
+#         kmeans = kMeans().clustering(features)
+#         dataset.addDatasetColumn('Appearance Cluster', kmeans.fit_predict(features))
+#         dataset.dropDatasetColumns(columnsToRemove=['radius_mean', 'texture_mean', 'perimeter_mean'])
+#         dataset.normalizeColumn('Appearance Cluster')
         
-    dataset.dropDatasetColumns(['Unnamed: 32'])
-    return dataset
+#     dataset.dropDatasetColumns(['Unnamed: 32'])
+#     return dataset
 
 def convertTime(unixTime):
     return datetime.fromtimestamp(unixTime/1000.0).strftime('%H:%M:%S %Y-%m-%d')
@@ -36,21 +34,21 @@ def extratDatasetName(data):
     end = dataString.find("'", start)
     return dataString[start:end]
 
-def inferModel(dataset : Dataset, modelInfo, X_test, y_test):
-    loadedModel = mlflow.pyfunc.load_model(modelInfo.model_uri)
-    predictions = loadedModel.predict(X_test)
-    featureNames = dataset.getDataset().columns.tolist()
-    result = pd.DataFrame(X_test, columns = featureNames)
+# def inferModel(dataset : Dataset, modelInfo, X_test, y_test):
+#     loadedModel = mlflow.pyfunc.load_model(modelInfo.model_uri)
+#     predictions = loadedModel.predict(X_test)
+#     featureNames = dataset.getDataset().columns.tolist()
+#     result = pd.DataFrame(X_test, columns = featureNames)
     
-    if y_test.ndim > 1 and y_test.shape[1] > 1:
-        y_test = np.argmax(y_test, axis=1)
+#     if y_test.ndim > 1 and y_test.shape[1] > 1:
+#         y_test = np.argmax(y_test, axis=1)
         
-    if predictions.ndim > 1 and predictions.shape[1] > 1:
-        predictions = np.argmax(predictions, axis=1)
+#     if predictions.ndim > 1 and predictions.shape[1] > 1:
+#         predictions = np.argmax(predictions, axis=1)
     
-    result["actual_class"] = y_test
-    result["predicted_class"] = predictions
-    result.sample(100).to_csv('src/Utils/predictions.csv', index=False)
+#     result["actual_class"] = y_test
+#     result["predicted_class"] = predictions
+#     result.sample(100).to_csv('src/Utils/predictions.csv', index=False)
 
 def getPath(data):
     part = data.get("modelName").replace(" ", "")
